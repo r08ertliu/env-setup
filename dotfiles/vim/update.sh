a6baa802f1ede01b2e2295f820b7434c61ccf132
Robert Liu <robert.liu@graidtech.com>
2024-05-23 23:46:05 +0800

    vim: Update branch name of cscope-fzf in update.sh

--- dotfiles/vim/update.sh
+++ dotfiles/vim/update.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+PREFIX="plugged"
+
 # Update all plug-in to remote master head
-for dir in plugged/*/ ; do
+for dir in ${PREFIX}/*/ ; do
 	pushd ${dir}
-	git pull origin master
+	if [ ${dir} == "${PREFIX}/cscope-fzf/" ]; then
+		git pull origin main
+	else
+		git pull origin master
+	fi
 	popd
 done
 
